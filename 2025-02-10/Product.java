c71b139b0c0bbcb7836defd351b445f3618d3bab
Cesinha <116749515+Cesinhaaaa@users.noreply.github.com>
2025-02-17 15:40:34 -0300

    Update Product.java
    code of class Product

--- 2025-02-10/Product.java
+++ 2025-02-10/Product.java
@@ -1,1 +1,10 @@
+public class Product {
+    private String name;
+    private double price;
 
+    public getName() { return this.name; }
+    public setName(String name) { this.name = name; }
+    
+    public getPrice() { return this.price; }
+    public setPrice(double price) { this.price = price; }
+}
