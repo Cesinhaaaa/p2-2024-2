196fe70dd006b7617cd6fd784207d7d68d583967
Cesinha <caiocesar35452@gmail.com>
2025-04-02 13:48:41 -0300

    Javadoc added

---
+++ jackut/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"br.ufal.ic.p2.jackut"},{"l":"br.ufal.ic.p2.jackut.code"},{"l":"br.ufal.ic.p2.jackut.exceptions"}];updateSearchResults();
